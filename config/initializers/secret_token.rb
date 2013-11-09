cfb97082be0de17e3bed754a5c95e3ce277db6d5
Tim Friedrich <friedrich.tim@googlemail.com>
2013-11-09 21:56:23 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GDS::Application.config.secret_key_base = 'c5168e2b8c90d9a933a0a06fe0d6eddb8c2f0ce736b2de8ab6c00acfb4f59c36b4caed7b9505acec339024bc699bca9f0fd92fe6f76bfc1568f3b2d6ca155e22'
