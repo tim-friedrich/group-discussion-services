4a50586e88e73a8211e86ba06a4260f5139378bb
Tim Friedrich <friedrich.tim@googlemail.com>
2013-11-15 19:16:43 +0100

    User password and signup

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "User pages" do
+	subject { page }
+
+	describe "signup" do
+		before { visit signup_path }
+		let(:submit) { "Create my account" }
+	
+		describe "with valid information" do
+			before do
+				fill_in "FirstName", 		with: "Example"
+				fill_in "Surname", 			with: "User"
+				fill_in "Email",				with:	"user@example.com"
+				fill_in "Password",			with: "foobar"
+				fill_in "Confirmation",	with:	"foobar"
+			end
+	
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
+	end
+end
