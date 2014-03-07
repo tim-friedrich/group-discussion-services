9940a92498276d99112afa571001c0e99b7dc35a
Tim Friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2014-03-07 16:03:21 +0100

    ..

---
+++ spec/models/factory_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+FactoryGirl.factories.map(&:name).each do |factory_name|
+
+  describe "factory #{factory_name}" do
+    before do
+      User.delete_all
+    end
+    
+    it 'is valid' do
+      factory = FactoryGirl.build(factory_name)
+
+      if factory.respond_to?(:valid?)
+        expect(factory).to be_valid, lambda { factory.errors.full_messages.join(',') } # the lamba syntax only works with rspec 2.14 or newer;  for earlier versions, you have to call #valid? before calling the matcher, otherwise the errors will be empty
+      end
+    end
+  end
+
+  describe 'with trait' do
+    before do
+      User.delete_all
+    end
+    FactoryGirl.factories[factory_name].definition.defined_traits.map(&:name).each do |trait_name|
+      it "is valid with trait #{trait_name}" do
+        factory = FactoryGirl.build(factory_name, trait_name)
+          
+        if factory.respond_to?(:valid?)
+          expect(factory).to be_valid, lambda { factory.errors.full_messages.join(',') } # see above
+        end
+      end
+    end
+  end
+end
