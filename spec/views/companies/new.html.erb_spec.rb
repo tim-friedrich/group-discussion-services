f331ee2bb2e18a81bfe154d08cbd0107eecd421a
Tim Friedrich <friedrich.tim@googlemail.com>
2014-09-18 16:07:46 +0200

    smaller fixes

--- spec/views/companies/new.html.erb_spec.rb
+++ spec/views/companies/new.html.erb_spec.rb
@@ -6,14 +6,7 @@ describe "companies/new" do
       :name => "",
       :contact_id => FactoryGirl.create(:contact).id
     ).as_new_record)
+
   end
 
-  it "renders new company form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", companies_path, "post" do
-      assert_select "input#company_name[name=?]", "company[name]"
-    end
-  end
 end
