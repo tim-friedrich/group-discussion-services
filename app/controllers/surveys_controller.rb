15cff1fed94ce17a82a29b9534fe91841d07edfb
Jan Lelis <mail@janlelis.de>
2015-01-28 17:50:48 +0100

    associate survey result with user

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,10 +1,30 @@
 class SurveysController < ApplicationController
   layout 'survey'
 
+
+  before_filter :authenticate_user!
+
+
   def new
+    # TODO don't allow if test result is already present for user
   end
 
   def create
-    render text: '\o/'
+    create_survey_result(survey_params[:results], current_user)
+    render text: '\o/', status: 201
+  end
+
+
+  private
+
+  def create_survey_result(results, user)
+    # code
+    puts "---------------------------------CREATING-SURVEY-----------------"
+    puts "-----------------------------------USER: #{user.username}"
+    p results
+  end
+
+  def survey_params
+    params.require(:survey).permit! # TODO more detailed
   end
 end
