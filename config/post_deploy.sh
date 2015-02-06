e05a0d6e9ef45442200b20102a6c4a41ffcab88e
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2015-02-06 16:14:27 +0100

    deploy

--- config/post_deploy.sh
+++ config/post_deploy.sh
@@ -1,10 +1,7 @@
 echo "## SET RUBY ##"
 source /usr/local/share/chruby/chruby.sh
 chruby 2.2.0
-ruby -v
-which ruby
-bundle -v
-
-gem -v
+gem install rake
+rake -v
 echo "## BUNDLE ##"
 bundle install --deployment --without test:development
