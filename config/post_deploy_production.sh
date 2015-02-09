6b0f166d785a256b8c75848234b37426b4b18837
Jan Lelis <mail@janlelis.de>
2015-02-09 15:57:11 +0100

    tweak post deploy scripts

--- config/post_deploy_production.sh
+++ config/post_deploy_production.sh
@@ -2,9 +2,12 @@
 echo "## SET RUBY ##"
 source /usr/local/share/chruby/chruby.sh
 chruby 2.2.0
+ruby -v
 echo "## BUNDLE ##"
 bundle install --deployment --without test:development
 echo "## PRECOMPILE ASSETS ##"
 bundle exec dotenv rake assets:precompile
+echo "## MIGRATE DATABASE ##"
+bundle exec dotenv rake db:migrate
 echo "## RESTART SERVER ##"
 sudo /etc/init.d/thin-production restart
