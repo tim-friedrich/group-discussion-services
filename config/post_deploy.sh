28ec0791f591431fc80b0c789fb0b85750600ebb
Jan Lelis <mail@janlelis.de>
2015-02-09 13:56:18 +0100

    add restarting thin server for staging env

--- config/post_deploy.sh
+++ config/post_deploy.sh
@@ -5,3 +5,5 @@ echo "## BUNDLE ##"
 bundle install --deployment --without test:development
 echo "## PRECOMPILE ASSETS ##"
 bundle exec dotenv rake assets:precompile
+echo "## RESTART SERVER ##"
+sudo /etc/init.d/thin-staging restart
