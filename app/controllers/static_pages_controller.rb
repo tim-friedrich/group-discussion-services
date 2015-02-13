7072382eeb30d7394f801c30cec36d87f647d3e0
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2015-02-13 12:58:54 +0100

    fixed static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,23 @@
 class StaticPagesController < ApplicationController
+  def send_contact_mail
+    ContactMailer.contact_us(params[:sender_name], params[:sender_email][0], params[:content]).deliver
+  end
+
+  def home
+    if current_user
+      redirect_to '/profile'
+    end
+  end
+
+  def contact_us
+
+  end
+
+  def impress
+
+  end
+
+  def contact_us_params
+    params.require(:contact_us).permit(:sender_name, :sender_email, :content)
+  end
 end
