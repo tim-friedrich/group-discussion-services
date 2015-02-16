5a2e6df9a6b3c41ead5d01a5427c7f8db701ccc6
Jan Lelis <mail@janlelis.de>
2015-02-16 14:48:01 +0100

    remove manual .delete_all statements and test style

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -1,19 +1,9 @@
 require 'spec_helper'
-#    t.string   "topic"
-#    t.integer  "moderator_id"
-#    t.datetime "due_date"
-#    t.datetime "created_at"
-#    t.datetime "updated_at"
-#    t.integer  "current_question_id"
-#    t.integer  "company_id"
 
 describe Discussion do
-  before do
-    User.delete_all
-    @discussion = FactoryGirl.build(:discussion)
-  end
+  let(:discussion){ FactoryGirl.build(:discussion) }
 
-  subject { @discussion }
+  subject { discussion }
   it { should respond_to(:topic) }
   it { should respond_to(:moderator) }
   it { should respond_to(:due_date) }
@@ -23,22 +13,22 @@ describe Discussion do
   it { should be_valid }
 
   describe "when due_date is not present" do
-    before { @discussion.due_date = nil }
+    before { discussion.due_date = nil }
     it { should_not be_valid }
   end
 
   it "should return the new current question if a new question is created" do
-    question = FactoryGirl.create(:question, discussion: @discussion)
-    @discussion.current_question.should eq question
+    question = FactoryGirl.create(:question, discussion: discussion)
+    expect( discussion.current_question ).to eq question
   end
 
   it "should return the research institute that the discussion belongs to" do
-    @research_institute = FactoryGirl.create(:research_institute)
-    @moderator = FactoryGirl.create(:user, role: Role.where(name: 'moderator').first)
-    @discussion.save
-    @moderator.research_institutes << @research_institute
-    @discussion.moderator = @moderator
-    @discussion.research_institute.should eq @research_institute
+    research_institute = FactoryGirl.create(:research_institute)
+    moderator = create(:moderator)
+    discussion.save
+    moderator.research_institutes << research_institute
+    discussion.moderator = moderator
+    expect( discussion.research_institute ).to eq research_institute
   end
 
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,24 +1,21 @@
 require 'spec_helper'
 
 describe Question do
-  before do
-    User.delete_all
-    @question = FactoryGirl.build(:question)
-  end
+  let(:question){ FactoryGirl.build(:question) }
 
-  subject { @question }
+  subject { question }
 
   it { should respond_to(:topic) }
   it { should respond_to(:discussion) }
   it { should be_valid }
 
   it "should be invalid when topic is not present" do
-    @question.topic = nil
+    question.topic = nil
     should_not be_valid
   end
 
   it "should be invalid when discussion is not present" do
-    @question.discussion_id = nil
+    question.discussion_id = nil
     should_not be_valid
   end
 end
