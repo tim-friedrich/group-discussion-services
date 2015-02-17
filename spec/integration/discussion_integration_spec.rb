0a46b453a3a113c08fd9010f3533955cbb0af027
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2015-02-17 16:46:34 +0100

    discussion integration tests

--- spec/integration/discussion_integration_spec.rb
+++ spec/integration/discussion_integration_spec.rb
@@ -3,46 +3,63 @@ require 'integration/integration_helpers'
 
 describe 'discussion', js: true do
   let(:user){ create(:user_with_survey) }
-
-  before do
-    @discussion = create(:discussion)
-    login_as @discussion.moderator
-
-    visit "/discussions/#{@discussion.id}"
+  let(:discussion){ create(:discussion) }
+  after do
+    # after each test navigate away before Capybara tries to so that we can appropriately handle the onbeforeunload dialog
+    if page.driver.browser
+      begin
+        page.driver.browser.navigate.to("about:blank")
+        page.driver.browser.switch_to.alert.accept
+      rescue Selenium::WebDriver::Error::NoAlertPresentError
+        # No alert was present. Don't need to do anything
+      end
+    end
   end
-  describe "new argument" do
-    after do
-      # after each test navigate away before Capybara tries to so that we can appropriately handle the onbeforeunload dialog
-      if page.driver.browser
-        begin
-          page.driver.browser.navigate.to("about:blank")
-          page.driver.browser.switch_to.alert.accept
-          rescue Selenium::WebDriver::Error::NoAlertPresentError
-          # No alert was present. Don't need to do anything
+  before do
+    discussion.users << user
+  end
+  def send_argument
+    page.execute_script("
+            $('#argument_content').text('test')
+          ")
+    page.find('#new_argument_button').click
+  end
+  describe "as Proband" do
+    describe "that is confirmed" do
+      before do
+        discussions_user = discussion.discussions_users.select{ | obj | obj.user.id == user.id }.first
+        discussions_user.confirmed = true
+        discussions_user.save
+        login_as user
+        visit "/discussions/#{ discussion.id }"
+      end
+      describe "post new argument" do
+        it "should post a new argument" do
+          send_argument
+          expect( page ).to have_content "test"
+        end
+
+        it "should clear the argument form after submit" do
+            send_argument
+            page.execute_script("
+              $('#argument_content').text('test')
+            ")
+            page.find('#new_argument_button').click
+            expect( find('#argument_content') ).not_to have_content "test"
         end
       end
     end
-    it "should post a new argument" do
-
-        page.execute_script("
-          $('#argument_content').text('test')
-        ")
-        page.find('#new_argument_button').click
-        sleep
-        expect( page ).to have_content "test"
-    end
-
-    it "should clear the argument form after submit" do
-
-        page.execute_script("
-          $('#argument_content').text('test')
-        ")
-        page.find('#new_argument_button').click
-        expect( find('#argument_content') ).not_to have_content "test"
+    describe "that is not confirmed" do
 
     end
+  end
 
-    #moderators
+  describe "as Moderator" do
+    before do
+      login_as discussion.moderator
+      visit "/discussions/#{ discussion.id }"
+    end
+
     it "should post a new question" do
     end
   end
