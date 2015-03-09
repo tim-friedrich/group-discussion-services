c6be0b335d6e3ff300c9b00a8269693f6fe1c1e5
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2015-03-09 13:16:40 +0100

    semi functional slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,7 @@
 //= require Chart.js/Chart.js
 //= require angularjs
 //= require angular-smart-table
-//= require angular-nouislider
-//= require nouislider
+//= require angular-slider
 
 // VENDOR
 //= require jquery.caret
