8b69f7ca7202ee939999c57d100a64f7bb8a4dae
Jan Lelis <mail@janlelis.de>
2015-03-11 16:25:49 +0100

    simpler spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 ENV["RAILS_ENV"] ||= 'test'
 
-require File.expand_path("../../config/environment", __FILE__)
-require File.expand_path("../../db/seeds", __FILE__)
+require_relative "../config/environment"
+require_relative "../db/seeds"
+
 require 'rspec/rails'
-require 'factory_girl_rails'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 Rails.application.routes.default_url_options[:host] = 'localhost:5000'
@@ -13,10 +13,8 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include Rails.application.routes.url_helpers
-  config.filter_run_excluding broken: true
 end
 
