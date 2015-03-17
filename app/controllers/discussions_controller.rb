tim-friedrich/group-discussion-services

commit ed8f3ac84698603fa5452c6720f148e1799dfdf6

delete some discussions

app/controllers/discussions_controller.rb
@@ -1,6 +1,5 @@
 class DiscussionsController < ApplicationController
   before_action :set_discussion, only: [:leave, :show, :edit, :update, :destroy, :evaluate, :arguments]
-  before_action :new_discussion, only: [ :create ]
   before_filter :authenticate_user!
 
 
@@ -53,10 +52,11 @@ class DiscussionsController < ApplicationController
   end
 
   def new
-    @discussion = Discussion.new
+    @discussion = Discussion.new(discussion_params)
+    @question = Question.create(topic: "Herzlich Willkommen", discussion: @discussion)
     @users = User.all
     @proband = DiscussionsUser.new
-    if current_user.research_institutes.first()
+    if current_user.research_institutes.first
       @companies = current_user.research_institutes.first.companies
     else
       @companies = []
@@ -145,11 +145,6 @@ class DiscussionsController < ApplicationController
 
   def set_discussion
     @discussion = Discussion.find(params[:id])
-  end
-
-  def new_discussion
-    @discussion = Discussion.new(discussion_params)
-    @question = Question.create(topic: "Herzlich Willkommen", discussion: @discussion)
   end
 
   def discussion_params

config/routes.rb
@@ -5,7 +5,7 @@ GDS::Application.routes.draw do
   resources :votes, only: [:create]
   resources :dislikes, only: [:create]
   resources :likes, only: [:create]
-  resources :discussions
+  resources :discussions, except: [:index]
   resources :questions, only: [:create]
   resources :arguments, only: [:create]
   resources :discussions_users

spec/controllers/discussions_controller_spec.rb
@@ -178,11 +178,6 @@ RSpec.describe DiscussionsController, :type => :controller do
     end
     context "should be redirected to the signin page" do
       describe "get" do
-        it "index" do
-          get :index, {}, valid_session
-          puts new_user_session_url
-          response.should redirect_to(new_user_session_url)
-        end
         it "show" do
           get :show, {:id => @discussion.to_param}
           response.should redirect_to(new_user_session_url)

commit 43e27aa3a865f20965ff1d6fca22a45993bd5254

fix what i broke with ed8f3ac84698603fa5452c6720f148e1799dfdf6

app/controllers/discussions_controller.rb
@@ -52,8 +52,7 @@ class DiscussionsController < ApplicationController
   end
 
   def new
-    @discussion = Discussion.new(discussion_params)
-    @question = Question.create(topic: "Herzlich Willkommen", discussion: @discussion)
+    @discussion = Discussion.new
     @users = User.all
     @proband = DiscussionsUser.new
     if current_user.research_institutes.first
@@ -90,6 +89,9 @@ class DiscussionsController < ApplicationController
   end
 
   def create
+    @discussion = Discussion.new(discussion_params)
+    @question = Question.create(topic: "Herzlich Willkommen", discussion: @discussion)
+
     respond_to do |format|
       if @discussion.save
         @discussion.moderator = current_user