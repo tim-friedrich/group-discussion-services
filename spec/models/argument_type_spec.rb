145933f22261adb9bb1f1a27aa5de1459051320c
Jan Lelis <mail@janlelis.de>
2015-04-02 15:46:54 +0200

    improve role and argument type

--- spec/models/argument_type_spec.rb
+++ spec/models/argument_type_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
-RSpec.describe ArgumentType, :type => :model do
-  let(:argument_type) { ArgumentType.find_by(name:'proband') }
 
-  it { argument_type.should respond_to(:name) }
+RSpec.describe ArgumentType, :type => :model do
+  it 'has proband record' do
+    expect( ArgumentType.proband ).to be_a ArgumentType
+  end
+
+  it 'has observer record' do
+    expect( ArgumentType.observer ).to be_a ArgumentType
+  end
+
+  it 'has moderator record' do
+    expect( ArgumentType.moderator ).to be_a ArgumentType
+  end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
+
 RSpec.describe Role, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has user record' do
+    expect( Role.user ).to be_a Role
+  end
+
+  it 'has proband record' do
+    expect( Role.proband ).to be_a Role
+  end
+
+  it 'has observer record' do
+    expect( Role.observer ).to be_a Role
+  end
+
+  it 'has moderator record' do
+    expect( Role.moderator ).to be_a Role
+  end
+
+  it 'has deputy record' do
+    expect( Role.deputy ).to be_a Role
+  end
+
+  it 'has admin record' do
+    expect( Role.admin ).to be_a Role
+  end
 end
